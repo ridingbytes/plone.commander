653a5c8ea2488fe10c121f63fd15d7c74d026e6c
Ramon Bartl <rb@ridingbytes.com>
2015-11-30 11:01:49 +0100

    updated Sencha Cmd
    Cmd Version 6.0.2.14

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BASE=~/develop/plone/plone.commander
+CMD_VERSION=6.0.2.14
 
-export CMD=/Users/rbartl/develop/js/Sencha/Cmd/6.0.0.202
-alias electron="static/Electron.app/Contents/MacOS/Electron static"
+export CMD=~/develop/js/Sencha/Cmd/$CMD_VERSION
+alias electron="$BASE/static/Electron.app/Contents/MacOS/Electron $BASE/static"
